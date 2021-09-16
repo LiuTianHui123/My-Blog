684f72f407eed7d3be3a7b7aa8c47b15c8ddf163
刘添慧 <945004066@qq.com>
2021-09-16 15:56:02 +0800

    home内容部门学习

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,9 @@ export const state = {
   loading: false,
   UserList: [111, 222, 333], //用户列表
   themeObj: {
-    autograph: 'Write the Code. Change the World.'
+    autograph: 'Write the Code. Change the World.',//控制首页头像下方问题
+    center_smailimg: '',//控制首页右侧图片
+    user_start: 1,//控制首页右侧图片文字
   },//主题
   aboutmeObj: '',//关于我的信息
   host: "http://" + window.location.host + "/port/",//接口路径
